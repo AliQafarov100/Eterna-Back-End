52c6150865adc70feae62f3669ea028be36d824a
Ali <tu8cvbxnx@code.edu.az>
2022-06-10 23:24:34 +0400

    Initial commit

---
+++ Eterna Back-End/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eterna_Back_End.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
